7ec6d70fca0071120f6b5c07ab4be3ae0825e6c7
Luis Hermida <luis.hn@zoho.com>
2020-08-19 13:34:14 -0500

    Warm up challenges

--- js/sock-merchant.js
+++ js/sock-merchant.js
@@ -10,6 +10,11 @@ function sockMerchant(n, ar) {
     return pairCounter;
 }
 
+function sockMerchant(n, ar) {
+    ar.sort();
+
+}
+
 console.log(sockMerchant(5, [10, 20, 20, 10, 20])) // 2
 console.log(sockMerchant(10, [10, 20, 20, 10, 20, 30, 40, 50, 60, 40])) // 3
 console.log(sockMerchant(20, [10, 20, 20, 10, 20, 30, 40, 50, 60, 40, 30, 40, 50, 60, 40, 30, 40, 50, 60, 40])) // 8
