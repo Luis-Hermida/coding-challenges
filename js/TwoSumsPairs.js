b7d6c0eb8e2aa60956381275153379418b381bb2
Luis Alejandro Hermida <42404237+Luis-Hermida@users.noreply.github.com>
2022-06-30 16:03:39 -0500

    Create TwoSumsPairs.js

---
+++ js/TwoSumsPairs.js
@@ -0,0 +1,50 @@
+// Sums equals 60
+function playlist(songs) {
+  const songPairs = new Map();
+  const target = 60;
+  let numberOfPairs = 0;
+
+  songs.forEach((song) => {
+    const searchingValue = target - song;
+
+    if (songPairs.has(searchingValue)) {
+      numberOfPairs += songPairs.get(searchingValue);
+    }
+
+    songPairs.set(song, songPairs.has(song) ? songPairs.get(song) + 1 : 1);
+  });
+
+  console.log(songPairs);
+  console.log("xd", numberOfPairs);
+}
+
+// playlist([10, 50, 20, 40]); // 2
+// playlist([30, 20, 30, 40, 40]); // 3
+// playlist([30, 30, 30, 30, 30]); // 10
+
+// Sums is divisible by 60
+function playlist(songs) {
+  const songPairs = new Map();
+  const target = 60;
+  let numberOfPairs = 0;
+
+  songs.forEach((song) => {
+    const currentSong = song % 60;
+    const searchingValue = (target - currentSong) % 60;
+
+    if (songPairs.has(searchingValue)) {
+      numberOfPairs += songPairs.get(searchingValue);
+    }
+
+    songPairs.set(
+      currentSong,
+      songPairs.has(currentSong) ? songPairs.get(currentSong) + 1 : 1
+    );
+  });
+
+  return numberOfPairs;
+}
+
+playlist([10, 50, 20, 40]);
+// playlist([30, 20, 30, 40, 40]);
+// playlist([30, 30, 30, 30, 30]);
