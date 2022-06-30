c7bb35c9d04b391b7af41f0ea4602144aca22a42
Luis Alejandro Hermida <42404237+Luis-Hermida@users.noreply.github.com>
2022-06-30 06:57:22 -0500

    Added JavaTwoNums.java

--- java/JavaLoops2.java
+++ java/JavaLoops2.java
@@ -1,21 +1,22 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-class Solution{
-    public static void main(String []argh){
-        Scanner in = new Scanner(System.in);
-        int t=in.nextInt();
-        for(int i=0;i<t;i++){
-            int a = in.nextInt();
-            int b = in.nextInt();
-            int n = in.nextInt();
-            
-            for (int nIndex = 0; nIndex < n; nIndex++) {
-                a = a + (int) Math.pow(2, nIndex) * b;
-                System.out.print(a + " ");
-            }
-            System.out.println();
-        }
-        in.close();
+class Solution {
+
+  public static void main(String[] argh) {
+    Scanner in = new Scanner(System.in);
+    int t = in.nextInt();
+    for (int i = 0; i < t; i++) {
+      int a = in.nextInt();
+      int b = in.nextInt();
+      int n = in.nextInt();
+
+      for (int nIndex = 0; nIndex < n; nIndex++) {
+        a = a + (int) Math.pow(2, nIndex) * b;
+        System.out.print(a + " ");
+      }
+      System.out.println();
     }
+    in.close();
+  }
 }
