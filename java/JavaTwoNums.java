4b264caa79b7e07aa749ce3facd0cb2120da0bca
Luis Alejandro Hermida <42404237+Luis-Hermida@users.noreply.github.com>
2022-06-30 07:19:25 -0500

    Added JavaTwoSums.java better solution

--- java/JavaTwoNums.java
+++ java/JavaTwoNums.java
@@ -1,3 +1,4 @@
+// O(log (n ^ 2))
 class Solution {
 
   public int[] twoSum(int[] nums, int target) {
@@ -11,6 +12,25 @@ class Solution {
       }
     }
 
+    throw new IllegalArgumentException("no match");
+  }
+}
+
+// O(log (n))
+class Solution {
+
+  public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> num_map = new HashMap<>();
+
+    for (int i = 0; i < nums.length; i++) {
+      int missingValue = target - nums[i];
+
+      if (num_map.containsKey(missingValue)) {
+        return new int[] { num_map.get(missingValue), i };
+      }
+
+      num_map.put(nums[i], i);
+    }
     throw new IllegalArgumentException("no match");
   }
 }
