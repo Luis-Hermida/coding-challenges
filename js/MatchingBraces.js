ac75db94a7f8ccce959772644a4076f029fc8398
Luis Alejandro Hermida <42404237+Luis-Hermida@users.noreply.github.com>
2022-08-23 11:07:59 -0500

    Create MatchingBraces.js

---
+++ js/MatchingBraces.js
@@ -0,0 +1,29 @@
+function matchingBraces(bracesArray) {
+  const isBraceIndexBalanced = [];
+
+  bracesArray.forEach((braces) => {
+    isBraceIndexBalanced.push(isBracesBalanced(braces));
+  });
+
+  return isBraceIndexBalanced;
+}
+
+function isBracesBalanced(bracesString) {
+  const currentSetOfBraces = [];
+
+  for (const value of bracesString) {
+    if (value === "{" || value === "[" || value === "(") {
+      currentSetOfBraces.push(value);
+    } else if (value === "}" && currentSetOfBraces.pop() !== "{") {
+      return "NO";
+    } else if (value === "]" && currentSetOfBraces.pop() !== "[") {
+      return "NO";
+    } else if (value === ")" && currentSetOfBraces.pop() !== "(") {
+      return "NO";
+    }
+  }
+
+  return currentSetOfBraces.length > 0 ? "NO" : "YES";
+}
+
+console.log(matchingBraces(["{}[]()", "{[}]}"]));
