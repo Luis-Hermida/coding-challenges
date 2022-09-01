8e922c4a4cf1961b0b4b3e000b175dff7e167b2a
Luis Alejandro Hermida <42404237+Luis-Hermida@users.noreply.github.com>
2022-09-01 18:50:07 -0500

    Create running_sum_of_1d_array.ts

---
+++ prefix_sum/running_sum_of_1d_array.ts
@@ -0,0 +1,15 @@
+function runningSum(nums: number[]): number[] {
+  let currentSum = 0;
+  const sumsArray: number[] = [];
+
+  nums.forEach((num) => {
+    currentSum += num;
+    sumsArray.push(currentSum);
+  });
+
+  return sumsArray;
+}
+
+console.log(runningSum([1, 2, 3, 4])); // [ 1, 3, 6, 10 ]
+console.log(runningSum([1, 1, 1, 1, 1])); // [ 1, 2, 3, 4, 5 ]
+console.log(runningSum([3, 1, 2, 10, 1])); // [ 3, 4, 6, 16, 17 ]
