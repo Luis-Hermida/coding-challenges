550791663c7d7b2dde3442d9da8429dc9124c43d
Luis Alejandro Hermida <42404237+Luis-Hermida@users.noreply.github.com>
2023-02-08 13:16:55 -0600

    Create solution.ts

---
+++ hash_table/jewels_and_stones/solution.ts
@@ -0,0 +1,20 @@
+// Stats
+// Runtime - 63 ms beats 83.37%
+// Memory -  42.1mb beats 69.16%
+
+/*
+
+  Time Complexity: O(n)
+
+  Space Complexity: O(1)
+*/
+
+var numJewelsInStones = function (jewels, stones) {
+  let jewelCounter = 0;
+
+  [...stones].forEach((stone) => {
+    jewels.includes(stone) ? jewelCounter++ : null;
+  });
+
+  return jewelCounter;
+};
