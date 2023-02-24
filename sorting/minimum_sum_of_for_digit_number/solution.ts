9fe99db1ca8e00b46e0cc7fb98cbde15b5adccf2
Luis Alejandro Hermida <42404237+Luis-Hermida@users.noreply.github.com>
2023-02-24 12:24:15 -0600

    Create solution.ts

---
+++ sorting/minimum_sum_of_for_digit_number/solution.ts
@@ -0,0 +1,14 @@
+// Stats
+// Runtime - 51 ms beats 98.82%
+// Memory - 41.8 mb beats 82.69%
+
+/*
+
+  Time Complexity: O(1)
+
+  Space Complexity: O(1)
+*/
+var minimumSum = function (num) {
+  const temp = num.toString().split("").sort();
+  return parseInt(temp[0] + temp[2]) + parseInt(temp[1] + temp[3]);
+};
