23d914a46b7a7b909a28c65a70030fa2e29555b1
Luis Alejandro Hermida <42404237+Luis-Hermida@users.noreply.github.com>
2023-03-01 01:11:53 -0600

    Create solution.ts

---
+++ binary_search/find_target_indices_after_sorting_array/solution.ts
@@ -0,0 +1,32 @@
+// Stats
+// Runtime - 78 ms beats 30.71%
+// Memory - 44.1 mb beats 12.66%
+
+/*
+
+  Time Complexity: 【・_・?】
+
+  Space Complexity: 【・_・?】
+*/
+
+function binarySearch(lists, sorted, low, high, target) {
+  if (low > high) return;
+
+  const mid = low + Math.floor((high - low) / 2);
+
+  if (sorted[mid] === target) {
+    lists.push(mid);
+  }
+
+  binarySearch(lists, sorted, low, mid - 1, target);
+  binarySearch(lists, sorted, mid + 1, high, target);
+}
+
+var targetIndices = function (nums, target) {
+  let result = [];
+  nums.sort((a, b) => a - b);
+  if (!nums.includes(target)) return [];
+
+  binarySearch(result, nums, 0, nums.length - 1, target);
+  return result.sort((a, b) => a - b);
+};
