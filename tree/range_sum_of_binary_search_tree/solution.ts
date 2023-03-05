600d2ef0a1aa7333ca7bda3aba89c857dd4b8b76
Luis Alejandro Hermida <42404237+Luis-Hermida@users.noreply.github.com>
2023-03-05 08:05:30 -0600

    Create solution.ts

---
+++ tree/range_sum_of_binary_search_tree/solution.ts
@@ -0,0 +1,29 @@
+// Stats
+// Runtime - 240 ms beats 32.35%
+// Memory - 72.8 mb beats 92.84%
+
+/*
+
+  Time Complexity: O(log n)
+
+  Space Complexity: O(log n)
+*/
+var rangeSumBST = function (root, low, high) {
+  let sum = 0;
+
+  if (root === null) return sum;
+
+  if (root.val > low) {
+    sum += rangeSumBST(root.left, low, high);
+  }
+
+  if (root.val <= high && root.val >= low) {
+    sum += root.val;
+  }
+
+  if (root.val < high) {
+    sum += rangeSumBST(root.right, low, high);
+  }
+
+  return sum;
+};
