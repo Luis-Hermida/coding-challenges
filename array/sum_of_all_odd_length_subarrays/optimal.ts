8dbaaced44f8649105ebb7786b172e3b7ddbc6d2
Luis Alejandro Hermida <42404237+Luis-Hermida@users.noreply.github.com>
2023-10-23 16:14:14 -0600

    Array - Sum of all odd length subarray

---
+++ array/sum_of_all_odd_length_subarrays/optimal.ts
@@ -0,0 +1,29 @@
+// Stats
+// Runtime - 67 ms beats 85.52%
+// Memory - 41.3 mb beats 99.25%
+
+/*
+  Time Complexity: O(n^3)
+
+  Space Complexity: O(1)
+*/
+
+var sumOddLengthSubarrays = function (arr: number[]): number {
+  let arrayLength = arr.length;
+  let totalSum = 0;
+
+  for (let left = 0; left < arrayLength; left++) {
+    for (let right = left; right < arrayLength; right++) {
+      if ((right - left + 1) % 2 === 1) {
+        let currentSum = 0;
+        for (let index = left; index < right + 1; ++index) {
+          currentSum += arr[index];
+        }
+
+        totalSum += currentSum;
+      }
+    }
+  }
+
+  return totalSum;
+};
