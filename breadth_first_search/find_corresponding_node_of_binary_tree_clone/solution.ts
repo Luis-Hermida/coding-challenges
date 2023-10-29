2ca629f28af2d221f4ebba11f80068f185e1e888
Luis Alejandro Hermida <42404237+Luis-Hermida@users.noreply.github.com>
2023-10-29 16:29:53 -0600

    Create solution.ts

---
+++ breadth_first_search/find_corresponding_node_of_binary_tree_clone/solution.ts
@@ -0,0 +1,29 @@
+// Stats
+// Runtime - 307 ms beats 66.47%
+// Memory - 64.4 mb beats 66.77%
+
+/*
+
+  Time Complexity: O(V + E) - Where V stands for vertices and E stands for edges
+
+  Space Complexity: 
+*/
+var getTargetCopy = function (original, cloned, target) {
+  if (original == null) {
+    // Base case aka stop condition
+    // empty tree or empty node
+    return null;
+  }
+
+  // General cases
+  if (original == target) {
+    // current original node is target, so is cloned
+    return cloned;
+  }
+
+  // Either left subtree has target, or right subtree has target
+  return (
+    getTargetCopy(original.left, cloned.left, target) ||
+    getTargetCopy(original.right, cloned.right, target)
+  );
+};
